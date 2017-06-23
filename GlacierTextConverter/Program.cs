651f6f0eebd38e44ac630991940ba8d4407b3788
majomix <mario.csaplar@centrum.sk>
2017-06-23 17:45:54 +0200

    Added loading directory of text files.

--- GlacierTextConverter/Program.cs
+++ GlacierTextConverter/Program.cs
@@ -22,8 +22,10 @@ namespace GlacierTextConverter
             //converter.WriteTextFile(@"F:\Hitman data\dl0patch1patch_LOCR_txt\");
             //converter.LoadDatFolder(@"D:\Hitman data\dlc0_LOCR\", new FileVersionSpecificationsBase());
             //converter.WriteTextFile(@"F:\Hitman data\dlc0_LOCR_txt\");
-            converter.LoadDatFolder(@"D:\Hitman data\chunk0_LOCR\", new FileVersionSpecificationsBase());
-            converter.WriteTextFile(@"F:\Hitman data\chunk0_LOCR_txt\");
+            //converter.LoadDatFolder(@"D:\Hitman data\chunk0_LOCR\", new FileVersionSpecificationsBase());
+            //converter.WriteTextFile(@"F:\Hitman data\chunk0_LOCR_txt\");
+
+            converter.LoadTextFolder(@"F:\Hitman data\chunk0patch1_LOCR_txt");
         }
     }
 }
