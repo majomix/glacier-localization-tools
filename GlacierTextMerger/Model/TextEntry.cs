ee4e6c2d01f31101c332e41a08bd5933539d3c73
Mario Csaplar <mario.csaplar@centrum.sk>
2017-12-28 16:52:51 +0100

    Added text merger. Merges many files into text file without duplicates.

---
+++ GlacierTextMerger/Model/TextEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GlacierTextMerger.Model
+{
+    public class TextEntry
+    {
+        public uint Hash { get; set; }
+        public string Entry { get; set; }
+        public List<string> Flags { get; private set; }
+
+        public TextEntry()
+        {
+            Flags = new List<string>();
+        }
+    }
+}
---
+++ GlacierTextMerger/Model/TextSection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GlacierTextMerger.Model
+{
+    public class TextSection
+    {
+        public List<TextEntry> Entries { get; private set; }
+        public string Name { get; set; }
+
+        public TextSection()
+        {
+            Entries = new List<TextEntry>();
+        }
+    }
+}
