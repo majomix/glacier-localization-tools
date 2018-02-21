c3c6903ef4bf95930974a3003cce018f15812749
Mario Csaplar <mario.csaplar@centrum.sk>
2018-02-21 19:38:38 +0100

    Functional changes. Added DLGE loading and text matching.

--- GlacierTextConverter/Model/TextConverter.cs
+++ GlacierTextConverter/Model/TextConverter.cs
@@ -1,39 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Linq;
+using System.Text;
 
 namespace GlacierTextConverter.Model
 {
     public class TextConverter
     {
-        public List<DatTextFile> Files { get; set; }
+        public List<LocrTextFile> LocrFiles { get; private set; }
+        public List<DlgeTextFile> DlgeFiles { get; private set; }
+        public Dictionary<UInt32, string> Replacement { get; private set; }
 
-        public void LoadDatFolder(string directory, IFileVersionSpecifications specifications)
+        public TextConverter()
         {
-            Files = new List<DatTextFile>();
-
-            foreach (string filePath in Directory.GetFiles(directory, "*", SearchOption.AllDirectories))
-            {
-                try
-                {
-                    Files.Add(new DatTextFile() { LanguageSections = LoadDatFile(filePath, specifications), Name = Path.GetFileName(filePath) });
-                    break;
-                }
-                catch (Exception e) { Console.WriteLine(filePath); }
-            }
+            LocrFiles = new List<LocrTextFile>();
+            DlgeFiles = new List<DlgeTextFile>();
+            Replacement = new Dictionary<UInt32, string>();
         }
 
-        public List<LanguageSection> LoadDatFile(string path, IFileVersionSpecifications specifications)
+        public void LoadLocrFolder(string directory)
         {
-            List<LanguageSection> languageSections = new List<LanguageSection>();
+            LoadGameDataFolder(directory, filePath => LocrFiles.Add(LoadLocrFile(filePath)));
+        }
 
-            using (FileStream fileStream = File.Open(path, FileMode.Open))
+        public void LoadDlgeFolder(string directory)
+        {
+            LoadGameDataFolder(directory, filePath => DlgeFiles.Add(new DlgeTextFile() { Structure = LoadDlgeFile(filePath), Name = Path.GetFileName(filePath) }));
+        }
+
+        public LocrTextFile LoadLocrFile(string path)
+        {
+            LocrTextFile file = null;
+
+            using (GlacierLocrBinaryReader reader = new GlacierLocrBinaryReader(File.Open(path, FileMode.Open), Encoding.Unicode))
             {
-                GlacierBinaryReader reader = new GlacierBinaryReader(fileStream, Encoding.Unicode, specifications.CypherStrategy);
+                List<LanguageSection> languageSections = new List<LanguageSection>();
 
-                for (int i = 0; i < specifications.NumberOfLanguages; i++)
+                for (int i = 0; i < reader.NumberOfLanguages; i++)
                 {
                     LanguageSection section = new LanguageSection();
                     section.StartingOffset = reader.ReadUInt32();
@@ -41,7 +45,7 @@ namespace GlacierTextConverter.Model
                     languageSections.Add(section);
                 }
 
-                for (int i = 0; i < specifications.NumberOfLanguages; i++)
+                for (int i = 0; i < reader.NumberOfLanguages; i++)
                 {
                     LanguageSection section = languageSections[i];
                     if(section.StartingOffset != uint.MaxValue)
@@ -53,171 +57,189 @@ namespace GlacierTextConverter.Model
                             TextEntry entry = new TextEntry();
                             entry.Hash = reader.ReadUInt32();
                             entry.Entry = reader.ReadString();
-                            break;
                             section.Entries.Add(entry);
                         }
                     }
-                    break;
+                }
+
+                file = new LocrTextFile()
+                {
+                    Name = Path.GetFileName(path),
+                    LanguageSections = languageSections,
+                    CypherStrategy = reader.CypherStrategy
+                };
+            }
+
+            return file;
+        }
+
+        public DlgeStructure LoadDlgeFile(string path)
+        {
+            DlgeStructure structure = null;
+
+            using (GlacierDlgeBinaryReader reader = new GlacierDlgeBinaryReader(File.Open(path, FileMode.Open), Encoding.Unicode))
+            {
+                int numberOfLanguages = 12;
+                ICypherStrategy cypherStrategy = new CypherStrategyXXTEA();
+
+                structure = reader.ReadStructure();
+                int nonEmptyStrings = 0;
+
+                for (int i = 0; i < numberOfLanguages; i++)
+                {
+                    structure.Dialogues[i] = reader.ReadString(cypherStrategy);
+
+                    if(!string.IsNullOrEmpty(structure.Dialogues[i]))
+                    {
+                        nonEmptyStrings++;
+                    }
+
+                    if (i != numberOfLanguages - 1)
+                    {
+                        if(reader.ReadLanguageMetadataAndDetermineIfEmpty())
+                        {
+                            i++;
+                        }
+                    }
+                }
+
+                if(nonEmptyStrings == 0)
+                {
+                    throw new InvalidDataException("No text inside dialogue file.");
                 }
             }
 
-            return languageSections;
+            return structure;
         }
 
         public void LoadTextFolder(string directory)
         {
-            Files = new List<DatTextFile>();
+            using (StreamReader reader = new StreamReader(File.Open(directory + @"\Slovak.txt", FileMode.Open), Encoding.UTF8))
+            {
+                string line = null;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split(new Char[] { '\t' }, 2);
+
+                    if (tokens.Count() == 2)
+                    {
+                        foreach(var identifier in tokens[0].Split(new Char[] { ',' }))
+                        {
+                            Replacement[Convert.ToUInt32(identifier, 16)] = tokens[1];
+                        }
+                    }
+                }
+            }
+        }
+
+        public void WriteCombinedTextFile(string directory)
+        {
+            if(LocrFiles == null || DlgeFiles == null) return;
+
+            Directory.CreateDirectory(directory);
 
             foreach (var languagePair in GetLanguageMap())
             {
-                using (FileStream fileStream = File.Open(directory + @"\" + languagePair.Value + ".txt", FileMode.Open))
+                using (StreamWriter writer = new StreamWriter(File.Open(directory + @"\" + languagePair.Key + ".txt", FileMode.Create), Encoding.UTF8))
                 {
-                    using (StreamReader reader = new StreamReader(fileStream, Encoding.UTF8))
+                    foreach (var textEntry in 
+                        LocrFiles
+                            .Where(file => file.LanguageSections.Count > languagePair.Value.Item1)
+                            .SelectMany(_ => _.LanguageSections[languagePair.Value.Item1].Entries
+                                .Select(text => new { Identifier = text.Hash, Entry = text.Entry }))
+                        .Concat(DlgeFiles
+                            .Select(_ => new { Identifier = _.Structure.Identifier, Entry = _.Structure.Dialogues[languagePair.Value.Item2] }))
+                        .Where(entry => entry.Entry != null)
+                        .GroupBy(entry => entry.Entry))
                     {
-                        string line = null;
-
-                        LanguageSection currentSection = null;
-
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            // new section
-                            if (line.StartsWith("[") && line.EndsWith("]"))
-                            {
-                                currentSection = new LanguageSection();
-
-                                string filename = line.Split('[', ']')[1];
-
-                                DatTextFile file = Files.Where(_ => _.Name == filename).SingleOrDefault();
-
-                                if (file == null)
-                                {
-                                    file = new DatTextFile() { Name = filename, LanguageSections = new List<LanguageSection>() };
-                                    Files.Add(file);
-                                }
-
-                                file.LanguageSections.Add(currentSection);
-                            }
-                            else if (currentSection != null)
-                            {
-                                string[] tokens = line.Split(new Char[] { '\t' }, 2);
-
-                                if (tokens.Count() == 2)
-                                {
-                                    TextEntry entry = new TextEntry()
-                                    {
-                                        Hash = Convert.ToUInt32(tokens[0], 16),
-                                        Entry = tokens[1]
-                                    };
-                                    currentSection.Entries.Add(entry);
-                                }
-                            }
-                        }
+                        writer.Write(string.Join(",", textEntry.Select(entry => string.Format("{0:X8}", entry.Identifier)).Distinct()));
+                        writer.Write("\t");
+                        writer.WriteLine(textEntry.Key.Replace("\r\n", "\\n"));
                     }
                 }
             }
         }
 
-        public void WriteTextFile(string directory)
+        public void WriteDatFolder(string directory)
         {
-            if(Files == null) return;
+            if (LocrFiles == null) return;
 
             Directory.CreateDirectory(directory);
 
-            foreach(var languagePair in GetLanguageMap())
+            foreach(LocrTextFile file in LocrFiles)
             {
-                using (FileStream fileStream = File.Open(directory + @"\" + languagePair.Value + ".txt", FileMode.Create))
+                using (GlacierBinaryWriter writer = new GlacierBinaryWriter(File.Open(directory + @"\" + file.Name, FileMode.Create), Encoding.UTF8, file.CypherStrategy))
                 {
-                    using (StreamWriter writer = new StreamWriter(fileStream, Encoding.UTF8))
+                    foreach (var section in file.LanguageSections)
                     {
-                        foreach (DatTextFile datFile in Files)
+                        writer.Write((UInt32)0);
+                    }
+
+                    foreach (LanguageSection section in file.LanguageSections)
+                    {
+                        if (section.Entries.Count != 0)
                         {
-                            if (datFile.LanguageSections.Count > languagePair.Key)
+                            section.StartingOffset = (uint) writer.BaseStream.Position;
+
+                            writer.Write(section.Entries.Count);
+
+                            foreach (TextEntry entry in section.Entries)
                             {
-                                LanguageSection languageSection = datFile.LanguageSections[languagePair.Key];
+                                string finalString = Replacement.ContainsKey(entry.Hash) ? Replacement[entry.Hash] : entry.Entry;
 
-                                writer.WriteLine("[{0}]", datFile.Name);
-
-                                foreach (TextEntry textEntry in languageSection.Entries)
-                                {
-                                    writer.WriteLine("{0:X8}\t{1}", textEntry.Hash, textEntry.Entry.Replace("\r\n", "\\n"));
-                                }
-
-                                writer.WriteLine();
+                                writer.Write(entry.Hash);
+                                writer.Write(finalString.Replace("\\n", "\r\n"));
                             }
                         }
+                        else
+                        {
+                            section.StartingOffset = uint.MaxValue;
+                        }
+                    }
+
+                    writer.BaseStream.Seek(0, SeekOrigin.Begin);
+                    foreach (var section in file.LanguageSections)
+                    {
+                        writer.Write(section.StartingOffset);
                     }
                 }
             }
         }
 
-        public void WriteDatFolder(string directory, IFileVersionSpecifications specifications)
+        public Dictionary<string, Tuple<int, int>> GetLanguageMap()
         {
-            if (Files == null) return;
-
-            Directory.CreateDirectory(directory);
-
-            foreach(DatTextFile file in Files)
+            return new Dictionary<string, Tuple<int, int>>()
             {
-                using (FileStream fileStream = File.Open(directory + @"\" + file.Name, FileMode.Create))
-                {
-                    using (GlacierBinaryWriter writer = new GlacierBinaryWriter(fileStream, Encoding.UTF8, specifications.CypherStrategy))
-                    {
-                        for (int i = 0; i < specifications.NumberOfLanguages; i++)
-                        {
-                            writer.Write((UInt32)0);
-                        }
-
-                        for (int i = 0; i < specifications.NumberOfLanguages; i++)
-                        {
-                            LanguageSection section = file.LanguageSections[i];
-                            
-                            if (section.Entries.Count != 0)
-                            {
-                                section.StartingOffset = (uint) writer.BaseStream.Position;
-
-                                writer.Write(section.Entries.Count);
-
-                                foreach (TextEntry entry in section.Entries)
-                                {
-                                    writer.Write(entry.Hash);
-                                    writer.Write(entry.Entry);
-                                }
-                            }
-                            else
-                            {
-                                section.StartingOffset = uint.MaxValue;
-                            }
-                        }
-
-                        writer.BaseStream.Seek(0, SeekOrigin.Begin);
-                        for (int i = 0; i < specifications.NumberOfLanguages; i++)
-                        {
-                            LanguageSection section = file.LanguageSections[i];
-
-                            writer.Write(section.StartingOffset);
-                        }
-                    }
-                }
-            }
-        }
-
-        public Dictionary<int, string> GetLanguageMap()
-        {
-            return new Dictionary<int, string>()
-            {
-                { 0, "Reference" },
-                { 1, "English" },
-                { 2, "French" },
-                { 3, "Italian" },
-                { 4, "German" },
-                { 5, "Spanish" },
-                { 6, "Russian" },
-                { 7, "Mexican" },
-                { 8, "Portugese" },
-                { 9, "Polish" },
-                { 10, "Japanese" },
-                { 11, "Empty" }
+                { "Empty", new Tuple<int, int>(0, 10) },
+                { "English", new Tuple<int, int>(1, 0) },
+                { "French", new Tuple<int, int>(2, 1 )},
+                { "Italian", new Tuple<int, int>(3, 2) },
+                { "German", new Tuple<int, int>(4, 3) },
+                { "Spanish", new Tuple<int, int>(5, 4) },
+                { "Russian", new Tuple<int, int>(6, 5) },
+                { "Mexican", new Tuple<int, int>(7, 6) },
+                { "Portugese", new Tuple<int, int>(8, 7) },
+                { "Polish", new Tuple<int, int>(9, 8) },
+                { "Japanese", new Tuple<int, int>(10, 9) },
+                { "Chinese", new Tuple<int, int>(0, 11) },
             };
+        }
+
+        private void LoadGameDataFolder(string directory, Action<string> action)
+        {
+            foreach (string filePath in Directory.GetFiles(directory, "*", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    action(filePath);
+                }
+                catch (Exception e)
+                {
+                    File.Delete(filePath);
+                    Console.WriteLine(filePath);
+                }
+            }
         }
     }
 }
